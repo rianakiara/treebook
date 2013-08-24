a525f10d3c0de511d03c2ca93a9bc3ce18f4aaeb
ricardo <ricardo.ianakiara@performanz.com.br>
2013-08-24 17:33:33 -0300

    Added tests for curtom routes

---
+++ test/integration/custom_routes_test.rb
@@ -0,0 +1,14 @@
+require 'test_helper'
+
+class CustomRoutesTest < ActionDispatch::IntegrationTest
+  test "The /login route opens the login page" do
+  	get '/login'
+  	assert_response :success
+  end
+
+  test "The /logout route opens the logout page" do
+  	get '/logout'
+  	assert_response :redirect
+  	assert_redirect_to '/'
+  end
+end
