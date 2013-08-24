a39cb83bde00a0fe78df74de7b7a8641db5a3302
ricardo <ricardo.ianakiara@gmail.com>
2013-08-23 22:15:41 -0300

    realizados os testes

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,14 +6,20 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
 
   # Setup accessible (or protected) attributes for your model
-  attr_accessible :email, :password, :password_confirmation, :remember_me
-  # Include default devise modules. Others available are:
-  # :token_authenticatable, :confirmable,
-  # :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
-
-  # Setup accessible (or protected) attributes for your model
-  attr_accessible :email, :password, :password_confirmation, :remember_me, :first_name, :last_name, :profile_name
+  attr_accessible :email, :password, :password_confirmation, :remember_me, 
+                  :first_name, :last_name, :profile_name
   # attr_accessible :title, :body
+
+  validades :first_name, presence: true
+
+  validades :last_name, presence: true
+
+  validades :profile_name, presence: true,
+                           uniqueness: true
+
+  has_many :statuses
+
+  def full_name
+    first_name + " " + last_name
+  end
 end
--- test/unit/user_test.rb
+++ test/unit/user_test.rb
@@ -1,7 +1,35 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+	test "a user shoul enter a first name" do 
+		user = User.new
+		assert !user.save
+		assert !user.errors[:first_name].empty?
+	end
+
+	test "a user shoul enter a last name" do 
+		user = User.new
+		assert !user.save
+		assert !user.errors[:last_name].empty?
+	end
+
+	test "a user shoul enter a profile name" do 
+		user = User.new
+		assert !user.save
+		assert !user.errors[:profile_name].empty?
+	end
+
+	test "a user shoul have a unique profile name" do 
+		user = User.new
+		user.profile_name = users(:yudi).profile_name
+		user.email = 'ricardo.ianakiara@gmail.com'
+		user.first_name = 'Ricardo'
+		user.last_name = 'ianakiara'
+		user.password = "k2v8ptj21"
+		user.password_confirmation = "k2v8ptj21"
+
+		assert !user.save
+		assert !user.errors[:profile_name].empty?
+	end
 end
+ 
